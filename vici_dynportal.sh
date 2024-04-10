1c86987c72f68ebdd5b8a75d444040f8f3e062cd
ManishK <32355900+manish23k@users.noreply.github.com>
2024-04-10 15:07:47 +0530

    Update vici_dynportal.sh

--- vici_dynportal.sh
+++ vici_dynportal.sh
@@ -1,11 +1,14 @@
 vicidial-dynamic portal Debian 11
 https://manishkadiya.blogspot.com/2024/04/debian-11-vicidial-scratch-install.html
 
-cp VB-firewall /usr/bin/
-
-chmod +x /usr/bin/VB-firewall
-
+cd firewalld_201
 cp -r zones /etc/firewalld/
+cp -r ipsets /etc/firewalld/
+cd ..
+cp services/ /usr/lib/firewalld/
+
+# Copy Dynamic Portal files to web folder
+cp -r dynportal_201 /var/www/html/dynportal
 
 cp vicidial-ssl.conf /etc/apache2/sites-available/
 
@@ -16,3 +19,7 @@ cd /servcies
 cp *.xml /usr/lib/firewalld/services/
 
 cp -r dynamicportal /var/www/html/
+
+cd usr_share_vicibox-firewall_201
+cp VB-firewall.pl /usr/bin/
+chmod +x /usr/bin/VB-firewall.pl
